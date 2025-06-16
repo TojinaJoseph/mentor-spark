e42a61851f920241c8e2c6dfece49e03f21b237e
TojinaJoseph <tojinajoseph123@gmail.com>
2025-06-16 21:40:42 +0530

    MS-1000:added input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,31 @@
-const Input = () => {
-  return <div>Input</div>;
+import type React from "react";
+import { useState } from "react";
+import Styles from "./Input.module.scss";
+
+export interface InputProps {
+  type: string;
+  label: string;
+  name: string;
+  placeholder: string;
+}
+
+const Input: React.FC<InputProps> = ({ type, label, name, placeholder }) => {
+  const [inputValue, setValue] = useState("");
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div className={Styles.inputContainer}>
+      <label htmlFor="">{label}</label>
+      <input
+        placeholder={placeholder}
+        type={type}
+        value={inputValue}
+        onChange={(e) => handleChange(e)}
+        name={name}
+      />
+    </div>
+  );
 };
 
 export default Input;
